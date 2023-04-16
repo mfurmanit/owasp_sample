1057055218bccb5dc9dde2bb51e19d3353f5a3bc
Maciej Furman <maciek.furman.user@gmail.com>
2023-04-16 11:47:59 +0200

    Initial commit.

---
+++ src/main/java/pl/mfurman/owasp_sample/OwaspSampleApplication.java
@@ -0,0 +1,13 @@
+package pl.mfurman.owasp_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwaspSampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OwaspSampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/pl/mfurman/owasp_sample/OwaspSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mfurman.owasp_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwaspSampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
